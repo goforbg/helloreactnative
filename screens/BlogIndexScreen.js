73839f14ed938ea68d604807372b49d66c5d119e
goforbg <techydevil@gmail.com>
2021-03-22 14:23:36 +0530

    12.16 : A bit of styling

--- screens/BlogIndexScreen.js
+++ screens/BlogIndexScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { View, Text, StyleSheet, FlatList, Button } from "react-native";
 import { Context } from "../context/AppContext";
+import { Feather } from "@expo/vector-icons";
 
 const BlogIndexScreen = () => {
   const { state, addBlogPost } = useContext(Context);
@@ -14,13 +15,31 @@ const BlogIndexScreen = () => {
         data={state}
         keyExtractor={(blogPost) => blogPost.title}
         renderItem={({ item }) => {
-          return <Text>{item.title}</Text>;
+          return (
+            <View style={styles.container}>
+              <Text style={styles.title}>{item.title}</Text>{" "}
+              <Feather style={styles.delete} name="trash" />
+            </View>
+          );
         }}
       />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 10,
+    borderBottomWidth: 1,
+  },
+  title: {
+    fontSize: 18,
+  },
+  delete: {
+    fontSize: 24,
+  },
+});
 
 export default BlogIndexScreen;
